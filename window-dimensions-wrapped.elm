fc9560f5c7568c3a41d30155791a5614ff0a81f9
Luis Arias <luis@balsamiq.com>
2015-05-04 17:37:31 +0200

    Wrap a record with width and height fields instead of the tuple coming off of Window.dimensions.

--- window-dimensions-wrapped.elm
+++ window-dimensions-wrapped.elm
@@ -2,10 +2,13 @@ import Graphics.Element exposing (..)
 import Signal exposing (..)
 import Window
 
-type Event = WindowSize (Int, Int)
+type Event = WindowSize {width: Int, height: Int}
+
+dim2ws : (Int, Int) -> Event
+dim2ws (w, h) = WindowSize {width = w, height = h}
 
 windowSize : Signal Event
-windowSize = WindowSize <~ Window.dimensions
+windowSize = map dim2ws Window.dimensions
 
 main : Signal Element
 main = map show windowSize
